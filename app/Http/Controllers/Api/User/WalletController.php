ea1a37809c247cd572bd0661e6135d2e4d281dc3
KalistusAlvino <kalistus33@gmail.com>
2025-07-30 02:30:52 +0700

    Income Category: New File Path

--- app/Http/Controllers/Api/User/WalletController.php
+++ app/Http/Controllers/Api/User/WalletController.php
@@ -50,7 +50,7 @@ class WalletController extends Controller
                 //Store using storage or public path
                 Storage::disk('public')->putFileAs('images/income_categories', $image, $imageName);
                 //Store name
-                $incomeCategory->image = url('images/income_categories/' . $imageName);
+                $incomeCategory->image = url('storage/images/income_categories/' . $imageName);
                 $incomeCategory->save();
 
                 return response()->json([
